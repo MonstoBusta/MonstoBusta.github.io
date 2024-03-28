579a79b976fd5289dba9f664fd3aa7b9fc87a13d
MonstoBusta <119002070+MonstoBusta@users.noreply.github.com>
2024-03-28 16:36:12 -0500

    making it mine

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "MonstoBusta",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Sound Designer",
+    bio: "Hello! My name is Max. I go by "MonstoBusta" online.",
+    email: "monstobusta@gmail.com",
+    linkedin: "Maximus Kim",
+    github: "MonstoBusta",
     instagram: "",
   },
   projects: [
