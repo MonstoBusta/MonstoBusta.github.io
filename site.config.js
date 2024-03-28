1e57631167b1a383118dd096e77db2bb530be748
MonstoBusta <119002070+MonstoBusta@users.noreply.github.com>
2024-03-28 16:45:35 -0500

    cmon why isn't this working

--- site.config.js
+++ site.config.js
@@ -12,19 +12,19 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Maximus Kim Sound`,
+      href: "https://github.com/MonstoBusta/MyWebsiteAndBlog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "Maximus Kim Sound",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
